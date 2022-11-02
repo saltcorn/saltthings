0509e35ba2a3329959eb8dcc0c983e19aba00b6f
Tom Nielsen <tanielsen@gmail.com>
2022-11-02 17:00:35 +0000

    createHttpServer

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,17 +1,41 @@
 let nextPid = 0
 const mailboxes = {}
+const http = require("http");
 
-
+//testing: curl -X POST http://localhost:3125 -H 'Content-Type: application/json'  -d '[0, "hello", "world from CURL"]'
 const myNode = { nodeID: null, nodeLocators: {} }
 
 const createNode = (options = {}) => {
     myNode.nodeID = Math.floor(Math.random() * 16777215).toString(16);
 
+    if (options.createHttpServer) {
+        const requestListener = function (req, res) {
+            let body = [];
+            req.on('data', (chunk) => {
+                body.push(chunk);
+            }).on('end', () => {
+                body = Buffer.concat(body).toString();
+                console.log(body);
+                [pid, ...msg] = JSON.parse(body)
+                send({ pid, myNode }, ...msg)
+                res.writeHead(200);
+                res.end();
+            });
+
+        };
+        const host = options.createHttpServer.host;
+        const port = options.createHttpServer.port;
+        const server = http.createServer(requestListener);
+        server.listen(port, host, () => {
+            console.log(`Server is running on http://${host}:${port}`);
+        });
+        myNode.nodeLocators.http = `http://${host}:${port}`
+    }
     return ({ pid }, ...msg) => send({ pid, node: myNode }, ...msg)
 }
 
 const send = ({ pid, node }, ...msg) => {
-    if (node.nodeID !== myNode.nodeID) {
+    if (node && node.nodeID !== myNode.nodeID) {
 
         return
     }
@@ -63,15 +87,15 @@ const spawn = (f, options = {}) => {
     return { pid, node, send: (msg, arg) => send({ pid, node }, msg, arg) }
 }
 
-createNode()
+createNode({ createHttpServer: { port: 3125, host: "localhost" } })
 
 const A = spawn(async function () {
     console.log("A", this.pid);
     const m = await this.receive()
     console.log("A got", m);
     await this.loop({
-        hello() {
-            console.log("A got hello msg in loop");
+        hello(s) {
+            console.log("A got hello msg in loop with: " + s);
         },
         foo({ x, y }) {
             console.log("A got foo", x, y);
